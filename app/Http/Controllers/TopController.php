8bd41b2ee3711ae490e1db7fac65f5b638d5b1e5
ruseltayongski <ruseltayong@gmail.com>
2018-10-31 11:22:47 +0800

    PushUpdates

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -16,26 +16,9 @@ class TopController extends Controller
 
     public function index(){
 
-        /*$user = User::limit(20)->get();
-        foreach($user as $row){
-            //$target = Barangay::where('province_id','=',$row->province)->where('muncity_id','=',$row->muncity)->sum('target');
-            $year2017 = \DB::connection('mysql')->select("select * from profile p where p.muncity_id = '$row->muncity' and p.province_id = '$row->province' and substring_index(substring_index(p.familyID, '-', 2),'-',-1) = '$row->id' and p.created_at like '%2017%' ");
-            $year2018 = \DB::connection('mysql')->select("select * from profile p where p.muncity_id = '$row->muncity' and p.province_id = '$row->province' and substring_index(substring_index(p.familyID, '-', 2),'-',-1) = '$row->id' and p.created_at like '%2018%' ");
-            return response()->json($year2017[0]->barangay_id,200, [],JSON_NUMERIC_CHECK);
-            $data = [
-                $row->id,
-                $row->fname.' '.$row->mname.' '.$row->lname,
-                $row->username,
-                $row->contact,
-                Province::find($row->province)->description,
-                Muncity::find($row->muncity)->description,
-                $target,
-                count($year2017),
-                count($year2018)
-            ];
-
-            $sheet->appendRow($data);
-        }*/
+        ini_set('max_execution_time', 0);
+        ini_set('memory_limit','99999999M');
+        ini_set('max_input_time','99999999999');
 
         \Excel::create('Top NDP', function($excel) {
             $excel->sheet('ALL', function($sheet)
