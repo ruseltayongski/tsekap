c9d891fc76f68a17ea6aeb7cbc75ecc7c3e05cdb
ruseltayongski <ruseltayong@gmail.com>
2018-11-07 14:16:38 +0800

    PushUpdates

--- app/Dengvaxia.php
+++ app/Dengvaxia.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dengvaxia extends Model
 {
-    protected $connection = 'dengvaxia_dummy';
+    protected $connection = 'dengvaxia';
     protected $table = 'dengvaxia_profiles';
     protected $guarded = array();
 }
