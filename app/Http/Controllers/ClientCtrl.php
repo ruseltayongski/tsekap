820f70850b9391aa5d85e349893d978578029e4a
Rusel T. Tayong <ruseltayong@gmail.com>
2020-05-11 09:58:35 +0800

    PushUpdates

--- app/Http/Controllers/ClientCtrl.php
+++ app/Http/Controllers/ClientCtrl.php
@@ -45,6 +45,7 @@ class ClientCtrl extends Controller
     {
         $muncity_id = Auth::user()->muncity;
         $user_priv = Auth::user()->user_priv;
+        $bherds_count = 0;
 
         if($user_priv==0){
             $countBarangay = Barangay::where('muncity_id',$muncity_id)->count();
