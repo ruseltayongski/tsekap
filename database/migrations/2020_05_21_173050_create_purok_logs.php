e103e815a58d408eb609eafdd96a421414119841
Rusel T. Tayong <ruseltayong@gmail.com>
2020-05-27 13:30:12 +0800

    PushUpdates

--- database/migrations/2020_05_21_173050_create_purok_logs.php
+++ database/migrations/2020_05_21_173050_create_purok_logs.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePurokDeleted extends Migration
+class CreatePurokLogs extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2020_05_21_173120_create_sitio_logs.php
+++ database/migrations/2020_05_21_173120_create_sitio_logs.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSitioDeleted extends Migration
+class CreateSitioLogs extends Migration
 {
     /**
      * Run the migrations.
