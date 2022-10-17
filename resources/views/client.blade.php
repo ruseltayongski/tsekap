fbf2bd4d11f7825eb8ae23bab96d57136d9b5fbc
ChristineAnne <christineannecatubig@gmail.com>
2022-10-17 08:48:59 +0800

    updated apk live

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -220,7 +220,7 @@ use App\Province;
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-mobile"></i> Mobile Check-Up<span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li>
-                            <a href="{{ asset("resources/apk/dummy.apk") }}">
+                            <a href="{{ asset("resources/apk/PHA-Check-Appv3.0.apk") }}">
                                 <i class="fa fa-download"></i> <small> Download APK</small>
                             </a>
                         </li>
