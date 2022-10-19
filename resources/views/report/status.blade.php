1fc2f7c3298c8f54d8a0f73af27f9a4113c04c42
ChristineAnne <christineannecatubig@gmail.com>
2022-10-19 16:44:38 +0800

    updated home dashboard, target population and status report

--- resources/views/report/status.blade.php
+++ resources/views/report/status.blade.php
@@ -72,40 +72,40 @@
                             $c++;
                             $target = $s->target;
                             Session::put('statreport_year', $year);
-                            $profile = Report::getProfile($level,$s->id);
-
-                            if($target==0){
-                                $target=$profile;
-                            }
-
-                            if($profile==0){
-                                $profilePercentage = 0;
-                            }else{
-                                $profilePercentage = ($profile / $target) * 100;
-                            }
-
-                            $a = $profilePercentage;
-                            $class = 'danger';
-                            if($a>=0 && $a<=20){
-                                $class = 'danger';
-                            }else if($a>20 && $a<=40){
-                                $class = 'warning';
-                            }else if($a>40 && $a<=60){
-                                $class = 'info';
-                            }else if($a>60 && $a<=80){
-                                $class = 'success';
-                            }else if($a>80){
-                                $class = 'aqua';
-                            }
+//                            $profile = Report::getProfile($level,$s->id);
+//
+//                            if($target==0){
+//                                $target=$profile;
+//                            }
+//
+//                            if($profile==0){
+//                                $profilePercentage = 0;
+//                            }else{
+//                                $profilePercentage = ($profile / $target) * 100;
+//                            }
+//
+//                            $a = $profilePercentage;
+//                            $class = 'danger';
+//                            if($a>=0 && $a<=20){
+//                                $class = 'danger';
+//                            }else if($a>20 && $a<=40){
+//                                $class = 'warning';
+//                            }else if($a>40 && $a<=60){
+//                                $class = 'info';
+//                            }else if($a>60 && $a<=80){
+//                                $class = 'success';
+//                            }else if($a>80){
+//                                $class = 'aqua';
+//                            }
                         ?>
                         <tr>
                             <td>{{ $s->description }} {{ $level }}</td>
 
                             <td class="text-center">{{ number_format($target) }}</td>
 
-                            <td class="text-center">{{ number_format($profile) }}</td>
+                            <td class="text-center"><span id="prov_profile{{ $s->id }}"><i class="fa fa-refresh fa-spin"></i></span></td>
 
-                            <td class="bg-{{$class}} text-center">{{ number_format($profilePercentage,1) }}%</td>
+                            <td class="bg-{{$class}} text-center"><span id="prov_percentage{{ $s->id }}"><i class="fa fa-refresh fa-spin"></i></span></td>
                             @if($level == 'province')
                             <form action="{{ asset('generatedownload') }}" method="POST">
                                 <td width="250px">
@@ -303,5 +303,38 @@
             $('#details_btn'+prov_id).attr('href','');
         }
     }
+
+    $.ajax({
+        url: "{{ asset('home/count/province/1/'.$year) }}",
+        type: 'GET',
+        success: function(result) {
+            $('#prov_profile1').html(result.countPopulation);
+            $('#prov_percentage1').html(result.profilePercentage + "%");
+        }
+    });
+    $.ajax({
+        url: "{{ asset('home/count/province/2/'.$year) }}",
+        type: 'GET',
+        success: function(result) {
+            $('#prov_profile2').html(result.countPopulation);
+            $('#prov_percentage2').html(result.profilePercentage + "%");
+        }
+    });
+    $.ajax({
+        url: "{{ asset('home/count/province/3/'.$year) }}",
+        type: 'GET',
+        success: function(result) {
+            $('#prov_profile3').html(result.countPopulation);
+            $('#prov_percentage3').html(result.profilePercentage + "%");
+        }
+    });
+    $.ajax({
+        url: "{{ asset('home/count/province/4/'.$year) }}",
+        type: 'GET',
+        success: function(result) {
+            $('#prov_profile4').html(result.countPopulation);
+            $('#prov_percentage4').html(result.profilePercentage + "%");
+        }
+    });
 </script>
 @endsection
