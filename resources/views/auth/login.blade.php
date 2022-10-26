532b06f22e24ff6202b997b00019d254d3248765
ChristineAnne <christineannecatubig@gmail.com>
2022-10-26 10:00:00 +0800

    changed number

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -58,7 +58,7 @@
                                     <li>HSDS Office - 260-9740 local 204</li>
                                     <li>Dr. Nelner D. Omus, HSDS Head - 09175748119</li>
                                     <li>Jane Michelle E. Raagas, DMO IV - 09173100611 </li>
-                                    <li>Prince Japee Neil C. Seno, PDO II - 099228947312</li>
+                                    <li>Prince Japee Neil C. Seno, PDO II - 09928947312</li>
                                     <li>Gian Ricki M. Bontilao, Nurse II - 09608522236</li>
                                 </ol>
                             </ol>
