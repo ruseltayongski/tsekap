a546911a2d4d030ee86629c01bb6866bf054cd34
ChristineAnne <christineannecatubig@gmail.com>
2023-04-26 09:21:09 +0800

    banner update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -99,7 +99,7 @@
     </div>
     <div class="header" style="background-color:#028482;padding:15px;">
         <div class="container">
-            <img src="{{ asset('resources/img/banner_2020.png') }}" class="img-responsive" />
+            <img src="{{ asset('resources/img/banner_tsekap2023v1.png') }}" class="img-responsive" />
         </div>
     </div>
     <div class="container">
