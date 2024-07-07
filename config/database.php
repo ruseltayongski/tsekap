872ee8d8d882925fda475b934ac2e8fa80b211c3
unknown <gonzalesjhondy>
2024-07-07 17:50:08 +0800

    adding input faileds in patient_form_blade

--- config/database.php
+++ config/database.php
@@ -10,9 +10,15 @@
 //$password = "adm1n";
 
 // dummy
-$host = "192.168.110.50";
-$username = "rtayong_dummy50";
-$password = "rtayong_dummy50";
+// $host = "192.168.110.50";
+// $username = "rtayong_dummy50";
+// $password = "rtayong_dummy50";
+
+//localhost
+
+$host = "localhost";
+$username = "root";
+$password = "";
 
 return [
 
