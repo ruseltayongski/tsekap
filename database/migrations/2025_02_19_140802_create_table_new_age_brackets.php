a56c8d7deaf571eb64d08a5878219046ceec0549
CDC RESU DOH-7, Elijah Nicholas Esguerra <doh7cdc.dev@gmail.com>
2025-02-19 14:31:57 +0800

    edited column naming for new_age_brackets

---
+++ database/migrations/2025_02_19_140802_create_table_new_age_brackets.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use Carbon\Carbon;
+
+class CreateTableNewAgeBrackets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('new_age_brackets'); // drop existing table for age brackets
+
+        // You may recreate the table here if needed
+        Schema::create('new_age_brackets', function (Blueprint $table) {
+            $table->integer('age_id')->autoIncrement();
+            $table->string('range');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+
+        DB::table('new_age_brackets')->insert([
+            ['range' => '20-29 years old', 'description' => 'Young Adult', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['range' => '30-39 years old', 'description' => 'Young Adult', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['range' => '40-49 years old', 'description' => 'Middle-aged Adult', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['range' => '50-59 years old', 'description' => 'Middle-aged Adult', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['range' => '60+ years old', 'description' => 'Senior Citizen', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // You may recreate the table here if needed
+        Schema::create('new_age_brackets', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('range');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+
+        DB::table('new_age_brackets')->insert([
+            ['range' => '20-29 years old', 'description' => 'Young Adult', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['range' => '30-39 years old', 'description' => 'Young Adult', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['range' => '40-49 years old', 'description' => 'Middle-aged Adult', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['range' => '50-59 years old', 'description' => 'Middle-aged Adult', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['range' => '60+ years old', 'description' => 'Senior Citizen', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
+    }
+}
